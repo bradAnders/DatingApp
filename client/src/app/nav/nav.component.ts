4b418d53f5a935bb379f8457bc5346442da55b92
Brad Anderson <btanders@pm.me>
2022-09-03 19:38:22 -0700

    Converted loggedIn to currentUser$ observable

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -8,34 +10,23 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {}
-  loggedIn: boolean;
+  currentUser$: Observable<User>;
 
   constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
-    this.getCurrentUser();
+    this.currentUser$ = this.accountService.currentUser$;
   }
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true;
       }, error: error => console.log(error)
     })
   }
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe({
-      next: user => {
-        this.loggedIn = !!user;
-      }, error: error => console.log(error)
-    });
-  }
-
 }
