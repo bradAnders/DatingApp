c10140b26f777c7555fa0d836285832871c149e8
Brad Anderson <btanders@pm.me>
2022-09-03 18:35:19 -0700

    Converted client calls to observer pattern

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -17,10 +17,9 @@ export class AppComponent implements OnInit {
   }
 
   getUsers() {
-    this.http.get('https://localhost:5001/api/Users/').subscribe(response => {
-      this.users = response;
-    }, error => {
-      console.log(error);
+    this.http.get('https://localhost:5001/api/Users/').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error)
     });
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -16,11 +16,11 @@ export class NavComponent implements OnInit {
   }
 
   login() {
-    this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
-      this.loggedIn = true;
-    }, error => {
-      console.log(error);
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true;
+      }, error: error => console.log(error)
     })
   }
 
