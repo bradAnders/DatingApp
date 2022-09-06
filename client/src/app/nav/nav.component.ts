a23f0b581003d4bafa76f8be8efc8f67b457c001
Brad Anderson <btanders@pm.me>
2022-09-05 18:09:48 -0700

    Added routing after login and logout

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../_services/account.service';
@@ -11,17 +12,24 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
     
   }
 
   login() {
-    this.accountService.login(this.model).subscribe()
+    this.accountService.login(this.model).subscribe({
+      next: _ => {
+        this.router.navigateByUrl('/members');
+      }, error: error => {
+        console.log(error);
+      }
+    })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/')
   }
 }
