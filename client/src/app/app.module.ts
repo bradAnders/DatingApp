4a15d6bc8e3398349d23b47b05686f5b7cda6181
Brad Anderson <btanders@pm.me>
2022-09-05 21:30:41 -0700

    Added server error view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 
 @NgModule({
@@ -31,6 +32,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     BrowserModule,
